058c305a32b03eb4b7a38cdbfddccf49ed8d34bc
Joyce Onor Odiagbe <uhzih@student.kit.edu>
2025-09-24 14:19:09 +0200

    tackeled keyword, default arguments, input functions

--- blockchain.py
+++ blockchain.py
@@ -1,17 +1,41 @@
+# Initializing our (empty) blockchain list
 blockchain = [0]
 
 
 def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain. """
     return blockchain[-1]
 
-
+# This function accepts two arguments.
+# One required one (transaction_amount) and one optional one (last_transaction)
+# The optional one is optional because it has a default value => [1]
 def add_value(transaction_amount):
+    """ Append a new value as well as the last blockchain value to the blockchain.
+    
+    Arguments:
+        :transaction_amount: The amount that should be added.
+        :last_transaction: The last blockchain transaction (default [1]).
+    """
     blockchain.append([get_last_blockchain_value(), transaction_amount])
-    
-    
-add_value(10.1)
-add_value(20.2)  # Call the function twice with different arguments
-add_value(30.3)
+
+def get_user_input():
+    """ Returns the input of the user (a new transaction amount) as a float. """
+    # Get the user input, transform it from a string to a float and store it in user_input
+    user_input = float(input('Your transaction amount please: '))
+    return user_input
+
+# Get the first transaction input and add the value to the blockchain
+tx_amount = get_user_input()
+add_value(tx_amount)
+
+# Get the second transaction input and add the value to the blockchain
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+
+# Get the third transaction input and add the value to the blockchain
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+
+# Output the blockchain list to the console
 
 print(blockchain)
-   
